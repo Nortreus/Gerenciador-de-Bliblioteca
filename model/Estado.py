b6bf8af21ec8666a716b4f083b91a0a144584ac4
Rafael Silveira Assunção <rafael.silveira.a12@gmail.com>
2023-07-22 15:02:25 -0300

    .gitIgnore

--- model/Estado.py
+++ model/Estado.py
@@ -1,8 +1,8 @@
 class Estado:
     _codigo = 0
-    def __init__(self,nome,cidade,responsavel):
+    def __init__(self,nome,responsavel):
         Estado._codigo +=1
         self._codigo = Estado._codigo
         self._nome = nome
-        self._cidade = cidade
+        self._cidade = []
         self._responsavel = responsavel
